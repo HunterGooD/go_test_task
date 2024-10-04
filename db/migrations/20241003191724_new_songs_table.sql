61ab89351081ebcefaa28778251cf5dabf8f16fa
HunterGooD <vlad.gudov@inbox.ru>
2024-10-04 20:08:59 +0400

    migrations changes

--- db/migrations/20241003191724_new_songs_table.sql
+++ db/migrations/20241003191724_new_songs_table.sql
@@ -1,12 +1,18 @@
 -- +goose Up
 -- +goose StatementBegin
+CREATE OR REPLACE FUNCTION update_update_at_column() RETURNS trigger LANGUAGE plpgsql AS $$ BEGIN NEW.update_at = (now() at time zone 'UTC'); RETURN NEW; END; $$;
+
 CREATE TABLE IF NOT EXISTS songs (
     id SERIAL PRIMARY KEY,
     m_name VARCHAR(100) NOT NULL,
     m_link VARCHAR(100) NOT NULL,
     m_text TEXT,
-    m_release_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    m_release_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE OR REPLACE TRIGGER update_data_systems_modtime BEFORE UPDATE ON songs FOR EACH ROW EXECUTE PROCEDURE update_update_at_column();
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE INDEX IF NOT EXISTS idx_text  ON songs USING gin (m_text gin_trgm_ops);
--- db/migrations/20241003191836_new_groups_table.sql
+++ db/migrations/20241003191836_new_groups_table.sql
@@ -1,9 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
+
 CREATE TABLE IF NOT EXISTS groups (
     id SERIAL PRIMARY KEY,
-    g_name VARCHAR(60) NOT NULL
+    g_name VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE OR REPLACE TRIGGER update_data_systems_modtime BEFORE UPDATE ON groups FOR EACH ROW EXECUTE PROCEDURE update_update_at_column();
 
 CREATE INDEX IF NOT EXISTS idx_group ON groups (g_name);
 -- +goose StatementEnd
